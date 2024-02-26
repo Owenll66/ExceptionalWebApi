8964f5d29bdfa7669dee35ae2280246cc4ec00eb
BowenLiu <bowen.liu@cba.com.au>
2024-02-26 14:00:31 +1030

    update

---
+++ WebApiExceptionTest/ExceptionType.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiExceptionTest
+{
+    public enum ExceptionType
+    {
+        InvalidException, // 400
+        UnauthorizedException, // 401
+        PaymentRquiredException, // 402
+        ForbiddenException, // 403
+        NotFoundException, // 404
+        InternalServerException // 500
+    }
+}
